ba82d9283197cea552e7af1134f62665be736b2d
DmitryYatsyuk-dv <dm.yatsyuk@gmail.com>
2021-08-03 13:08:47 +0300

    Framework added background gradient of 2 colors of your choice

---
+++ TestColorKit.h
@@ -0,0 +1,18 @@
+//
+//  TestColorKit.h
+//  TestColorKit
+//
+//  Created by Lucky on 03.08.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestColorKit.
+FOUNDATION_EXPORT double TestColorKitVersionNumber;
+
+//! Project version string for TestColorKit.
+FOUNDATION_EXPORT const unsigned char TestColorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestColorKit/PublicHeader.h>
+
+
---
+++ TestColorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  GBViewController.swift
+//  TestColorKit
+//
+//  Created by Lucky on 03.08.2021.
+//
+
+import UIKit
+
+open class GBViewController: UIViewController {
+    
+    public func setBackgroundGradient(first: UIColor, second: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.colors = [first.cgColor,
+                           second.cgColor]
+        gradient.locations = [0, 1]
+        view.layer.addSublayer(gradient)
+        gradient.frame = view.frame
+    }
+}
